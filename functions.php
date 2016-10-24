e7cf311e0f538201706c962c66945080d48e9581
acosmin <acosmin.com@gmail.com>
2016-10-24 10:20:49 +0300

    Updated TGMPA to v2.6.1
    And changed init function

--- functions.php
+++ functions.php
@@ -1301,62 +1301,47 @@ add_action( 'init', 'ac_compatibility_unreg_icons' );
 
 /*  TGM Setup & Plugins
 /* ------------------------------------ */
-add_action( 'tgmpa_register', 'my_theme_register_required_plugins' );
+if ( ! function_exists( 'ac_register_required_plugins' ) ) {
+	function ac_register_required_plugins() {
 
-function my_theme_register_required_plugins() {
+	    $plugins = array(
 
-    $plugins = array(
+			array(
+	            'name'      => 'Contact Form 7',
+	            'slug'      => 'contact-form-7',
+	            'required'  => false,
+	        ),
 
-		array(
-            'name'      => 'Contact Form 7',
-            'slug'      => 'contact-form-7',
-            'required'  => false,
-        ),
+			array(
+	            'name'      => 'Revive Old Post',
+	            'slug'      => 'tweet-old-post',
+	            'required'  => false,
+	        ),
 
-		array(
-            'name'      => 'Revive Old Post',
-            'slug'      => 'tweet-old-post',
-            'required'  => false,
-        ),
+			array(
+	            'name'      => 'WP Product Review',
+	            'slug'      => 'wp-product-review',
+	            'required'  => false,
+	        ),
 
-		array(
-            'name'      => 'WP Product Review',
-            'slug'      => 'wp-product-review',
-            'required'  => false,
-        ),
+	    );
 
-    );
+		$config = array(
+			'id'           => 'justwrite',                 // Unique ID for hashing notices for multiple instances of TGMPA.
+			'default_path' => '',                      // Default absolute path to bundled plugins.
+			'menu'         => 'tgmpa-install-plugins', // Menu slug.
+			'has_notices'  => true,                    // Show admin notices or not.
+			'dismissable'  => true,                    // If false, a user cannot dismiss the nag message.
+			'dismiss_msg'  => '',                      // If 'dismissable' is false, this message will be output at top of nag.
+			'is_automatic' => false,                   // Automatically activate plugins after installation or not.
+			'message'      => '',
+			'strings'      => array(
+				'notice_can_install_recommended'  => _n_noop( 'This theme recommends the following plugin: %1$s.', '<em><a class="ac-pro-theme-link" target="_blank" href="http://www.acosmin.com/theme/justwrite-pro/">JustWrite Pro with WooCommerce compatibility</a></em> in now available. Also, this theme recommends the following plugins: %1$s.', 'justwrite' ),
+			)
+		);
 
-    $config = array(
-        'default_path' => '',
-        'menu'         => 'tgmpa-install-plugins',
-        'has_notices'  => true,
-        'dismissable'  => true,
-        'dismiss_msg'  => '',
-        'is_automatic' => false,
-        'message'      => '',
-        'strings'      => array(
-            'page_title'                      => __( 'Install Required Plugins', 'justwrite' ),
-            'menu_title'                      => __( 'Install Plugins', 'justwrite' ),
-            'installing'                      => __( 'Installing Plugin: %s', 'justwrite' ),
-            'oops'                            => __( 'Something went wrong with the plugin API.', 'justwrite' ),
-            'notice_can_install_required'     => _n_noop( 'This theme requires the following plugin: %1$s.', 'This theme requires the following plugins: %1$s.', 'justwrite' ),
-            'notice_can_install_recommended'  => _n_noop( 'This theme recommends the following plugin: %1$s.', '<em><a class="ac-pro-theme-link" target="_blank" href="http://www.acosmin.com/theme/justwrite-pro/">JustWrite Pro with WooCommerce compatibility</a></em> in now available. Also, this theme recommends the following plugins: %1$s.', 'justwrite' ),
-            'notice_cannot_install'           => _n_noop( 'Sorry, but you do not have the correct permissions to install the %s plugin. Contact the administrator of this site for help on getting the plugin installed.', 'Sorry, but you do not have the correct permissions to install the %s plugins. Contact the administrator of this site for help on getting the plugins installed.', 'justwrite' ),
-            'notice_can_activate_required'    => _n_noop( 'The following required plugin is currently inactive: %1$s.', 'The following required plugins are currently inactive: %1$s.', 'justwrite' ),
-            'notice_can_activate_recommended' => _n_noop( 'The following recommended plugin is currently inactive: %1$s.', 'The following recommended plugins are currently inactive: %1$s.', 'justwrite' ),
-            'notice_cannot_activate'          => _n_noop( 'Sorry, but you do not have the correct permissions to activate the %s plugin. Contact the administrator of this site for help on getting the plugin activated.', 'Sorry, but you do not have the correct permissions to activate the %s plugins. Contact the administrator of this site for help on getting the plugins activated.', 'justwrite' ),
-            'notice_ask_to_update'            => _n_noop( 'The following plugin needs to be updated to its latest version to ensure maximum compatibility with this theme: %1$s.', 'The following plugins need to be updated to their latest version to ensure maximum compatibility with this theme: %1$s.', 'justwrite' ),
-            'notice_cannot_update'            => _n_noop( 'Sorry, but you do not have the correct permissions to update the %s plugin. Contact the administrator of this site for help on getting the plugin updated.', 'Sorry, but you do not have the correct permissions to update the %s plugins. Contact the administrator of this site for help on getting the plugins updated.', 'justwrite' ),
-            'install_link'                    => _n_noop( 'Begin installing plugin', 'Begin installing plugins', 'justwrite' ),
-            'activate_link'                   => _n_noop( 'Begin activating plugin', 'Begin activating plugins', 'justwrite' ),
-            'return'                          => __( 'Return to Required Plugins Installer', 'justwrite' ),
-            'plugin_activated'                => __( 'Plugin activated successfully.', 'justwrite' ),
-            'complete'                        => __( 'All plugins installed and activated successfully. %s', 'justwrite' ),
-            'nag_type'                        => 'updated'
-        )
-    );
+	    tgmpa( $plugins, $config );
 
-    tgmpa( $plugins, $config );
-
+	}
 }
+add_action( 'tgmpa_register', 'ac_register_required_plugins' );
